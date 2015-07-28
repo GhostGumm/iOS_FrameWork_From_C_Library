b612f01fcf20d71ac4af5ef595f7df100e75a49b
Hakim <elbakk_a@Mac-mini-de-Morvan.local>
2015-07-28 11:39:57 +0200

    C static Library into iOS FrameWork

---
+++ MosquittoIOs/MosquittoIOs/MosquittoIOs.h
@@ -0,0 +1,44 @@
+//
+//  MosquittoIOs.h
+//  MosquittoIOs
+//
+//  Created by Hakim on 27/07/15.
+//  Copyright (c) 2015 Hakim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MosquittoIOs.
+FOUNDATION_EXPORT double MosquittoIOsVersionNumber;
+
+//! Project version string for MosquittoIOs.
+FOUNDATION_EXPORT const unsigned char MosquittoIOsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MosquittoIOs/PublicHeader.h>
+#import <MosquittoIOs/hashtable.h>
+#import <MosquittoIOs/libZeta.h>
+#import <MosquittoIOs/2046.h>
+#import <MosquittoIOs/comet_buffer.h>
+#import <MosquittoIOs/comet_memory.h>
+#import <MosquittoIOs/comet_message.h>
+#import <MosquittoIOs/cometd.h>
+#import <MosquittoIOs/cometd_internal.h>
+#import <MosquittoIOs/CommandParsingInfo.h>
+#import <MosquittoIOs/getGdaInfo.h>
+#import <MosquittoIOs/groupUsersInfo.h>
+#import <MosquittoIOs/handler.h>
+#import <MosquittoIOs/HashMaps.h>
+#import <MosquittoIOs/listingEntryInfo.h>
+#import <MosquittoIOs/listingGdaInfo.h>
+#import <MosquittoIOs/messageEntryInfo.h>
+#import <MosquittoIOs/putGdaInfo.h>
+#import <MosquittoIOs/queueInfo.h>
+#import <MosquittoIOs/stacklistenerInfo.h>
+#import <MosquittoIOs/stackListingInfo.h>
+#import <MosquittoIOs/zetaclient.h>
+#import <MosquittoIOs/jansson.h>
+#import <MosquittoIOs/MosquittoIOS.h>
+#import <MosquittoIOs/ZetaInit+ZetaWrap.h>
+#import <MosquittoIOs/jansson_private.h>
+#import <MosquittoIOs/strbuffer.h>
+#import <MosquittoIOs/utf.h>
